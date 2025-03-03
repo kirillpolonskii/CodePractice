64a2a5475e452399640986c268e308acd2f86e87
Kirill Polonskii <polkirill2002@gmail.com>
2025-03-03 19:27:16 +0300

    Solve 1000 in greedy.

---
+++ kotlin/CodeforcesArchive/src/main/kotlin/greedy/compl_1000/2039B_Shohag_loves_string.kt
@@ -0,0 +1,33 @@
+package greedy.compl_1000
+
+fun main(){
+    val t = readln().toInt()
+    repeat(t){
+        val s = readln()
+        var found = false
+        for (i in 0..<s.length-1){
+            if (s[i] == s[i + 1]){
+                print(s[i])
+                print(s[i + 1])
+                found = true
+                break
+            }
+        }
+        if (!found){
+            for (i in 0..<s.length-2){
+                if (s[i] != s[i + 1] && s[i + 1] != s[i + 2] && s[i] != s[i + 2]){
+                    print(s[i])
+                    print(s[i + 1])
+                    print(s[i + 2])
+                    found = true
+                    break
+                }
+            }
+        }
+        if(!found)
+            print(-1)
+        println()
+    }
+
+
+}
