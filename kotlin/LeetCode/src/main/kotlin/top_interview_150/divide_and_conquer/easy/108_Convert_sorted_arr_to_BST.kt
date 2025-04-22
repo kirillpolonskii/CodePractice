eb4cba5b5190a287828905fb4e0939c33c6fed8c
Kirill Polonskii <polkirill2002@gmail.com>
2025-04-22 16:37:03 +0300

    Solve easy in Divide and Conquer.

---
+++ kotlin/LeetCode/src/main/kotlin/top_interview_150/divide_and_conquer/easy/108_Convert_sorted_arr_to_BST.kt
@@ -0,0 +1,31 @@
+package top_interview_150.divide_and_conquer.easy
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+class Solution {
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        return recursiveTransformation(nums)
+    }
+
+    private fun recursiveTransformation(nums: IntArray): TreeNode? {
+        if (nums.isEmpty()) return null
+        val curRoot = TreeNode(nums[nums.size / 2])
+        if (nums.size > 1) {
+            curRoot.left = recursiveTransformation(nums.copyOfRange(0, nums.size / 2))
+            curRoot.right = recursiveTransformation(nums.copyOfRange(nums.size / 2 + 1, nums.size))
+        }
+        return curRoot
+    }
+}
