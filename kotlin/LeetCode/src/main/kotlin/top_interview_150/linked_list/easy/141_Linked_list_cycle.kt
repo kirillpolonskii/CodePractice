7c21e9db895a66b062e0798ceaa9f19fde780ded
Kirill Polonskii <polkirill2002@gmail.com>
2025-07-04 19:03:20 +0300

    Solve easy in Linked List.

---
+++ kotlin/LeetCode/src/main/kotlin/top_interview_150/linked_list/easy/141_Linked_list_cycle.kt
@@ -0,0 +1,17 @@
+package top_interview_150.linked_list.easy
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+// идиотская задачка с идиотским условием
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+
+    }
+}
