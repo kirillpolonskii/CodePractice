8abdf4851a2a33211f828266063ba113b9fa432f
Kirill Polonskii <polkirill2002@gmail.com>
2025-08-14 15:31:50 +0300

    Almost solve hard in Array / String.

---
+++ kotlin/LeetCode/src/main/kotlin/top_interview_150/array_string/hard/135_Candy.kt
@@ -0,0 +1,26 @@
+package top_interview_150.array_string.hard
+
+class Solution135 {
+    fun candy(ratings: IntArray): Int {
+        val n = ratings.size
+        val candies = IntArray(n) { 1 }
+        for (i in 0..<n) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1
+            }
+            println(candies.joinToString())
+        }
+
+        for (i in n-1 downTo 0) {
+            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+                candies[i] = kotlin.math.max(candies[i], candies[i + 1] + 1)
+            }
+            println(candies.joinToString())
+        }
+        return candies.sum()
+    }
+}
+
+fun main(){
+    println(Solution135().candy(intArrayOf(3,2,1,1,4,3,3)))
+}
--- kotlin/LeetCode/src/main/kotlin/top_interview_150/linked_list/meduim/2_Add_two_nums.kt
+++ kotlin/LeetCode/src/main/kotlin/top_interview_150/linked_list/meduim/2_Add_two_nums.kt
@@ -1,17 +1,17 @@
 package top_interview_150.linked_list.meduim
 
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
+class ListNode2(var `val`: Int) {
+    var next: ListNode2? = null
 }
 
-class Solution {
-    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+class Solution2 {
+    fun addTwoNumbers(l1: ListNode2?, l2: ListNode2?): ListNode2? {
         var curL1 = l1
         var curL2 = l2
         var nextAddition = 0
         var curDigit = curL1!!.`val` + curL2!!.`val` + nextAddition
         println(curDigit)
-        val head = ListNode(if (curDigit > 9) curDigit - 10 else curDigit)
+        val head = ListNode2(if (curDigit > 9) curDigit - 10 else curDigit)
         var curSumL = head
         nextAddition = if (curDigit > 9) 1 else 0
         while (curL1?.next != null || curL2?.next != null){
@@ -20,24 +20,24 @@ class Solution {
                     (if (curL2?.next != null) curL2.next!!.`val` else 0)
             println(curDigit)
             nextAddition = if (curDigit > 9) 1 else 0
-            curSumL.next = ListNode(if (curDigit > 9) curDigit - 10 else curDigit)
+            curSumL.next = ListNode2(if (curDigit > 9) curDigit - 10 else curDigit)
             curSumL = curSumL.next!!
             curL1 = curL1?.next
             curL2 = curL2?.next
         }
         if (nextAddition == 1) {
-            curSumL.next = ListNode(1)
+            curSumL.next = ListNode2(1)
         }
         return head
     }
 }
 
 fun main(){
-    val head1 = ListNode(2)
-    head1.next = ListNode(4)
-    head1.next!!.next = ListNode(3)
-    val head2 = ListNode(5)
-    head2.next = ListNode(6)
-    head2.next!!.next = ListNode(4)
-    println(Solution().addTwoNumbers(head1, head2)?.`val`)
+    val head1 = ListNode2(2)
+    head1.next = ListNode2(4)
+    head1.next!!.next = ListNode2(3)
+    val head2 = ListNode2(5)
+    head2.next = ListNode2(6)
+    head2.next!!.next = ListNode2(4)
+    println(Solution2().addTwoNumbers(head1, head2)?.`val`)
 }
